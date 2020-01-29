e58441500c25d1e4ace3be17d4192c27579bf8be
chaffai13 <55139835+chaffai13@users.noreply.github.com>
2020-01-29 10:54:18 +0100

    Add files via upload

---
+++ src/main/java/com/wildCircus/Abdelwahhab/WildCircusAbdelwahhabApplication.java
@@ -0,0 +1,13 @@
+package com.wildCircus.Abdelwahhab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildCircusAbdelwahhabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildCircusAbdelwahhabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildCircus/Abdelwahhab/WildCircusAbdelwahhabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildCircus.Abdelwahhab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildCircusAbdelwahhabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
